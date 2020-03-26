779344f8c13fa01ac7acc9dd626171f3a9aa7662
kazu-toku <toku65535@gmail.com>
2020-03-26 18:38:24 +0900

    PinBallプロジェクト 　課題：得点を表示しよう
    UIのTextを使って得点の表示
    ターゲット（大小の星と雲）にボールが衝突した時に得点を加算する
    ターゲットの種類によって取得できる点数を変える
    得点は (ゲームビューの画面サイズを変えても常に) 画面の右上に見やすく表示する

---
+++ Assets/BallController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour
+{
+    //得点を表示するテキスト
+    public Text scoreText; //Text用変数
+    private int score = 0;
+
+    //ボールが見える可能性のあるz軸の最大値
+    private float visiblePosZ = -6.5f;
+     
+
+    //ゲームオーバを表示するテキスト
+    private GameObject gameoverText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //シーン中のGameOverTextオブジェクトを取得
+        this.gameoverText = GameObject.Find("GameOverText");
+
+        score = 0;
+        SetScore();   //初期スコアを代入して表示
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ボールが画面外に出た場合
+        if (this.transform.position.z < this.visiblePosZ)
+        {
+            //GameoverTextにゲームオーバを表示
+            this.gameoverText.GetComponent<Text>().text = "Game Over";
+        }
+        
+    }
+     void OnCollisionEnter(Collision collision)
+    {
+        // タグによって得点を変える
+        if (collision.gameObject.tag == "SmallStarTag")
+        {
+            score += 10;
+        }
+        else if (collision.gameObject.tag == "LargeStarTag")
+        {
+            score += 50;
+        }
+        else if (collision.gameObject.tag == "SmallCloudTag" || collision.gameObject.tag == "LargeCloudTag")
+        {
+            score += 5;
+        }
+            SetScore();
+        
+    }
+    void SetScore()
+    {
+        scoreText.text = string.Format("Score{0}" , score);
+    }
+}
